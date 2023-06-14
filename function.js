d941253af82c493d094770888edc72e5219a85f2
vingor77 <vingor76@gmail.com>
2023-06-14 16:02:14 -0400

    Added images slot

--- function.js
+++ function.js
@@ -1,6 +1,7 @@
 var entityCount = 113; //Total amount of entities.
 var nav = document.getElementById("nav");
 
+//TODO: Check if entity actually exists. Change to while loop and go through an array of numbers.
 for(var i = 0; i < entityCount; i++) {
     //Create the list element
     li = document.createElement("li");
@@ -22,12 +23,17 @@ function show(entity) {
     var div = document.getElementById('showImage');
 
     //Create the img element
-    var img = document.createElement("img");
-    img.setAttribute("src", "images/Entity_" + entity + ".png");
-    img.setAttribute("class", "justify-content-center");
+    var stats = document.createElement("img");
+    stats.setAttribute("src", "statBlocks/Entity_" + entity + ".png");
+    stats.setAttribute("class", "justify-content-center");
+
+    //Repeat steps above but for image.
+    var stats = document.createElement("img");
+    stats.setAttribute("src", "images/Entity_" + entity + ".png");
+    stats.setAttribute("class", "justify-content-center");
 
     div.innerText = ""; //Wipe previous image, then set the new one.
 
-    //Append image
-    div.appendChild(img);
+    //Append stat block
+    div.appendChild(stats);
 }
